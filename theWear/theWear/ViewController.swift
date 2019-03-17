c6b25cc212875e50bfb641603bf53c7fbdfce390
val0904 <val0904@yandex.ru>
2019-03-17 12:11:37 +0300

    Add files via upload

---
+++ theWear/theWear/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  theWear
+//
+//  Created by Валентина on 17/03/2019.
+//  Copyright © 2019 Валентина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
